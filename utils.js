f0c6b21cfec54ad1eefd2f0f0415b68843f1a72f
taichunmin <taichunmin@gmail.com>
2021-02-02 19:07:45 +0800

    save

--- utils.js
+++ utils.js
@@ -2,4 +2,4 @@ const _ = require('lodash')
 
 exports.getenv = (key, defaultval) => _.get(process, ['env', key], defaultval)
 
-exports.getBaseUrl = () => _.trimEnd(exports.getenv('BASEURL', 'https://localhost:3000/'), '/') + '/'
+exports.getBaseUrl = () => _.trimEnd(exports.getenv('BASEURL', 'https://2021taipeicycle.bikonnect.com/'), '/') + '/'
